fcbfd009c9802733524880070b911b4aba689074
max <max@navtech.in.th>
2016-05-04 14:23:04 +0700

    addd datatable crack show detail

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute'])
+angular.module('app', ['ngRoute','app.dtModule'])
     .controller('HomeController', HomeController)
     .controller('AboutController', AboutController)
     .config(function ($routeProvider) {
@@ -29,28 +29,93 @@ function AboutController($scope, $http) {
     
     $scope.SendData = function () {
             
-    $http.get("user/"+$scope.userId)
+    getData($scope, $http, $scope.userId)
+}
+}
+
+function getData(scope,http,id){
+    http.get("user/"+id)
     .then(function(response) {
         console.log("PostData...success")
-        $scope.id = response.data.id;
-        $scope.userName = response.data.userName;
-        $scope.name = response.data.name;
-        $scope.position = response.data.position;
+        scope.id = response.data.id;
+        scope.username = response.data.username;
+        scope.name = response.data.name;
+        scope.position = response.data.position;
     });
-    }
 }
 
 
-angular.module('ngDB',[])
+angular.module('app.dtModule',['datatables'])
     .controller('DatatableController', DatatableController)
+    .controller('DatatableController2', DatatableController2)
     .config(function ($routeProvider) {
         $routeProvider
         .when('/datatable', {
-                controller: 'DatatableController',
                 templateUrl: 'datatable.html'
+            })
+        .when('/datatable2', {
+                templateUrl: 'datatable2.html'
             })
      });
     
-    function DatatableController($scope, $http)  {
-    console.log("test");
+    
+    
+    
+    
+    function DatatableController($scope, $http, DTOptionsBuilder, DTColumnBuilder)  {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder.fromSource('users')
+        .withPaginationType('full_numbers')
+        .withOption('rowCallback', rowCallback);
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID'),
+        DTColumnBuilder.newColumn('username').withTitle('Username'),
+        DTColumnBuilder.newColumn('name').withTitle('Name'),
+        DTColumnBuilder.newColumn('position').withTitle('Position')
+    ];
+    
+    function someClickHandler(info) {
+        $scope.id = info.id;
+        $scope.username = info.username;
+        $scope.name = info.name;
+        $scope.position = info.position;
     }
+    
+    function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+        $('td', nRow).unbind('click');
+        $('td', nRow).bind('click', function() {
+            $scope.$apply(function() {
+                someClickHandler(aData);
+            });
+        });
+        return nRow;
+    }
+    }
+    
+    function DatatableController2($scope, $http, DTOptionsBuilder, DTColumnBuilder)  {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder.fromSource('users')
+        .withPaginationType('full_numbers')
+        .withOption('rowCallback', rowCallback);
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID'),
+        DTColumnBuilder.newColumn('username').withTitle('Username'),
+    ];
+    
+    function someClickHandler(info) {
+        getData($scope,$http,info.id);
+    }
+    
+    function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+        $('td', nRow).unbind('click');
+        $('td', nRow).bind('click', function() {
+            $scope.$apply(function() {
+                someClickHandler(aData);
+            });
+        });
+        return nRow;
+    }
+    }
+
+    
+    
